15ac75a6833b3e91d04b0e871c9109e7b5722379
unknown <hishmay42@yahoo.com>
2022-11-25 06:08:45 +0200

    create boilder server

--- project-1/server.js
+++ project-1/server.js
@@ -1,1 +1,12 @@
-console.log("Hello From Node");
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Welcome to the homepage")
+});
+app.get("/admin", (req, res) => {
+    res.send("This is the top secret admin page");
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server Listing on ${PORT}`);
+});
