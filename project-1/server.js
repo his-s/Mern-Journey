608cec8f137c28ffd8020b1c2e370b909144c277
unknown <hishmay42@yahoo.com>
2022-11-25 09:51:09 +0200

    react connected

--- project-1/server.js
+++ project-1/server.js
@@ -7,20 +7,26 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("public"));
 app.get("/", async (req, res) => {
-    const allAnimals = await db.collection("animals").find().toArray();
+  const allAnimals = await db.collection("animals").find().toArray();
 
-    res.render("home", { allAnimals });
+  res.render("home", { allAnimals });
 });
 app.get("/admin", (req, res) => {
-    res.render("admin");
+  res.render("admin");
+});
+app.get("/api/animals", async (req, res) => {
+  const allAnimals = await db.collection("animals").find().toArray();
+  res.json(allAnimals);
 });
 const PORT = 3000;
 async function start() {
-    const client = new MongoClient("mongodb://root:root@localhost:27017/AmazingMernApp?&authSource=admin");
-    await client.connect();
-    db = client.db();
-    app.listen(PORT, () => {
-        console.log(`Server Listing on ${PORT}`);
-    });
+  const client = new MongoClient(
+    "mongodb://root:root@localhost:27017/AmazingMernApp?&authSource=admin"
+  );
+  await client.connect();
+  db = client.db();
+  app.listen(PORT, () => {
+    console.log(`Server Listing on ${PORT}`);
+  });
 }
 start();
--- project-1/src/index.js
+++ project-1/src/index.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
+import Axios from "axios";
 function App() {
+  const [animals, setAnimals] = useState([]);
+  useEffect(() => {
+    async function start() {
+      const response = await Axios.get("/api/animals");
+      setAnimals((prev) => (prev = response.data));
+    }
+    start();
+  }, []);
   return (
     <div>
       <h1>Hello</h1>
       <p>From React</p>
+      {animals.map((animal) => (
+        <AnimalCard
+          name={animal.name}
+          species={animal.species}
+          key={animal._id}
+        ></AnimalCard>
+      ))}
     </div>
+  );
+}
+function AnimalCard(props) {
+  return (
+    <p>
+      Hi,My Name is {props.name} and I'm {props.species}
+    </p>
   );
 }
 const root = createRoot(document.querySelector("#app"));
