99083392ded16119fcda1bdf5e64fcc1a9fe6afe
unknown <hishmay42@yahoo.com>
2022-11-25 06:43:30 +0200

    connect mongodb

--- project-1/server.js
+++ project-1/server.js
@@ -1,4 +1,6 @@
+const { MongoClient } = require("mongodb");
 const express = require("express");
+let db;
 const app = express();
 app.get("/", (req, res) => {
     res.send("Welcome to the homepage")
@@ -7,6 +9,12 @@ app.get("/admin", (req, res) => {
     res.send("This is the top secret admin page");
 });
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server Listing on ${PORT}`);
-});
+async function start() {
+    const client = new MongoClient("mongodb://root:root@localhost:27017/AmazingMernApp?&authSource=admin");
+    await client.connect();
+    db = client.db();
+    app.listen(PORT, () => {
+        console.log(`Server Listing on ${PORT}`);
+    });
+}
+start();
